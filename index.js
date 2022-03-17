3927e7e3fdc1b12be64be30088e0cd3731c98496
Jacob Nordstrom <jnordstrom@64labs.com>
2022-03-17 15:13:13 -0400

    added the new Webhook API key, along with comments about what the code is doing.

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const { default: axios } = require('axios');
 require('dotenv').config();
 
+// variable that will get shuffled in below function.
 var namesArray = ['Jacob', 'Keano', 'Chris', 'Tanner', 'Kyle', 'Darek', 'Eric', 'Steve', 'Matias'];
 
+// Function that will shuffle the above array.
 function shuffleArray(array) {
   array.sort(() => Math.random() - 0.5);
 }
 shuffleArray(namesArray);
 
+// New shuffled array put in JSON format.
 var names = JSON.stringify(namesArray);
+
+// Formatting new JSON array
 names = names.replace(/['"]+/g, '').replace('[', '').replace(']', '').split(',').join('\n');
 
+// Function that will post the shuffled array into Slack.
 const main = async () => {
   try {
     const payload = {
